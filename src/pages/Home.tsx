4c40deac11863ba826ec5468cfc89e8a0376f404
Leo-yTnk <leonardo.yukio.tanaka@gmail.com>
2025-12-29 16:20:40 -0300

    .

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,22 @@
-import '../components/Aside/Aside.css'
+import "../styles/Aside.css"
 
 function Home() {
   return (
     <>
-      <aside></aside>
+      <header className="page-header">
+
+      </header>
+
+      <aside className="sidebar">
+        <header className="sidebar-header">
+          <button className="account">
+            <svg className="bi" width="32" height="32" fill="currentColor">
+              <use href="bootstrap-icons.svg#home"/>
+            </svg>
+          </button>
+        </header>
+      </aside>
+
       <main>
         <h1>Home</h1>
         <p>Bem-vindo ao projeto.</p>
