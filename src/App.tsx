86eec920e258a007c6c13961cf1e4563351ce885
Leo-yTnk <leonardo.yukio.tanaka@gmail.com>
2025-12-30 00:24:14 -0300

    Refactor header and sidebar components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home.tsx'
 import Notebooks from './pages/Notebooks.tsx'
+import Search from './pages/Search.tsx'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/notebooks" element={<Notebooks />}></Route>
+        <Route path="/search" element={<Search />}></Route>
       </Routes>
     </BrowserRouter>
   )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,64 +1,11 @@
-import "../styles/Aside.css"
-import Link from 'react-router-dom'
-import { 
-    Person, 
-    Gear, 
-    LayoutSidebar, 
-    House,
-    Search,
-    Journal
-} from "react-bootstrap-icons";
+import PageHeader from '../components/PageHeader'
+import Sidebar from '../components/Sidebar'
 
 function Home() {
   return (
     <>
-        <header className="page-header">
-
-        </header>
-
-        <aside className="sidebar">
-            <header className="sidebar-header">
-                <div>
-                    <a className="chrome-control settings">
-                        <Gear size={16} />
-                    </a>
-                    <a className="chrome-control account">
-                        <Person size={16} />
-                    </a>
-                </div>
-                <button className="chrome-control toggle-sidebar">
-                    <LayoutSidebar size={16} />
-                </button>
-            </header>
-
-            <nav className="sidebar-nav">
-                <ul className="chrome-control">
-                    <li>
-                        <a className="sidebar-nav-link" href="">
-                            <House />
-                            <span>Início</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a className="sidebar-nav-link" href="">
-                            <Search />
-                            <span>Buscar</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a className="sidebar-nav-link" href="">
-                            <Journal />
-                            <span>Cadernos</span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-
-            <hr />
-
-            <h6>Recentes</h6>
-
-        </aside>
+        <PageHeader />
+        <Sidebar />
 
         <main>
             <h1>Home</h1>
