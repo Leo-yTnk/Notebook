6869c8950da53029b57c6b040e97b1fc4f691341
Leo-yTnk <leonardo.yukio.tanaka@gmail.com>
2025-12-30 00:02:30 -0300

    .

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,63 @@
 import "../styles/Aside.css"
-import { Person, PersonFill } from "react-bootstrap-icons";
+import { 
+    Person, 
+    Gear, 
+    LayoutSidebar, 
+    House,
+    Search,
+    Journal
+} from "react-bootstrap-icons";
 
 function Home() {
   return (
     <>
-      <header className="page-header">
+        <header className="page-header">
 
-      </header>
-
-      <aside className="sidebar">
-        <header className="sidebar-header">
-          <button className="account">
-            <Person     size={32} className="icon outline-icon" />
-            <PersonFill size={32} className="icon fill-icon" />
-          </button>
         </header>
-      </aside>
 
-      <main>
-        <h1>Home</h1>
-        <p>Bem-vindo ao projeto.</p>
-      </main>
+        <aside className="sidebar">
+            <header className="sidebar-header">
+                <div>
+                    <a className="chrome-control settings">
+                        <Gear size={16} />
+                    </a>
+                    <a className="chrome-control account">
+                        <Person size={16} />
+                    </a>
+                </div>
+                <button className="chrome-control toggle-sidebar">
+                    <LayoutSidebar size={16} />
+                </button>
+            </header>
+
+            <nav className="sidebar-nav">
+                <ul className="chrome-control">
+                    <li>
+                        <a className="sidebar-nav-link" href="">
+                            <House />
+                            <span>Início</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a className="sidebar-nav-link" href="">
+                            <Search />
+                            <span>Buscar</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a className="sidebar-nav-link" href="">
+                            <Journal />
+                            <span>Cadernos</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </aside>
+
+        <main>
+            <h1>Home</h1>
+            <p>Bem-vindo ao projeto.</p>
+        </main>
     </>
   )
 }
