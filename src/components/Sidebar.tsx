5f0e890276cc695288637f6209078688962d8afc
Leo-yTnk <leonardo.yukio.tanaka@gmail.com>
2025-12-30 13:10:10 -0300

    Fix sidebar build errors

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -43,7 +43,7 @@ function Sidebar() {
         </button>
       </header>
 
-      <nav className="sidebar-nav" ref={setAnimatedElement(1)}>
+      <nav className="sidebar-nav">
         <ul className="chrome-control">
           <li>
             <Link className="sidebar-nav-link" to="/">
@@ -66,9 +66,7 @@ function Sidebar() {
         </ul>
       </nav>
 
-      <hr ref={setAnimatedElement(2)} />
-
-      <h6 ref={setAnimatedElement(3)}>Recentes</h6>
+      <hr />
 
       <h6>Recentes</h6>
 
