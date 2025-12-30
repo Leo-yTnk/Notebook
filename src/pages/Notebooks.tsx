8e6baf8df1b889efcd4660e246af81e9b56fe93b
Leo-yTnk <leonardo.yukio.tanaka@gmail.com>
2025-12-30 13:29:47 -0300

    Refine layout components structure

--- src/pages/Notebooks.tsx
+++ src/pages/Notebooks.tsx
@@ -2,16 +2,16 @@ import PageHeader from '../components/PageHeader'
 import Sidebar from '../components/Sidebar'
 
 function Notebooks() {
-    return (
-        <>
-            <PageHeader />
-            <Sidebar />
+  return (
+    <>
+      <PageHeader />
+      <Sidebar />
 
-            <main>
-            <h1>Cadernos</h1>
-            </main>
-        </>
-    )
+      <main>
+        <h1>Cadernos</h1>
+      </main>
+    </>
+  )
 }
 
 export default Notebooks
